600206849db2786fedbd0c9dfeef1bc16cd15ed2
Tania Karpenko <tania-karpenko@ukr.net>
2017-09-25 10:35:43 +0300

    add one more project

---
+++ dwl_campaign_page/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('.player_playlist.dwl_js_custom_scroll').mCustomScrollbar({
+    scrollButtons:{
+      enable:true
+    }
+  })
+  $('.dwl_cp_rightbar .rightbar_body.dwl_js_custom_scroll').mCustomScrollbar({
+    scrollButtons:{
+      enable:true
+    }
+  })
+  $('.dwl_cp_swatches_list ul.dwl_js_custom_scroll_horizontal').mCustomScrollbar({
+    scrollButtons:{
+      enable:true
+    },
+    horizontalScroll: true
+  })
+})
+  
